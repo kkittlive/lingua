34e09730defd54f0cb6a07a9c4b35c81f5154338
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-02-25 16:01:31 -0300

    Added status column to lessons table. Also validated the inclusion in an array of 4 values: pending, confirmed, completed, canceled (all lower case)

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,5 +2,6 @@ class Lesson < ApplicationRecord
   belongs_to  :teacher, class_name: "User"
   belongs_to  :student, class_name: "User"
   has_many :reviews
+  validates :status, inclusion: {in: %w[pending confirmed completed canceled]}
   validates_presence_of :date, :location, :teacher, :student
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_165952) do
+ActiveRecord::Schema.define(version: 2020_02_25_184407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_165952) do
     t.bigint "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "qualifications", force: :cascade do |t|

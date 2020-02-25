96384177859a85c16055a21908487b0620e364f1
Janette Kwan <kkitt@live.com>
2020-02-25 17:18:34 -0300

    updated User table with has many and removed language column from user table and removed language element from seed hash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_192510) do
+ActiveRecord::Schema.define(version: 2020_02_25_201451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_192510) do
     t.string "phone_number"
     t.integer "age"
     t.string "gender"
-    t.string "language"
     t.string "location"
     t.string "availability"
     t.text "biography"

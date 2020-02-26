f7677b048fa2ffb80e222398fee54d1115529c6a
Emilie Drop <emilie.drop@gmx.fr>
2020-02-26 18:40:52 -0300

    search functionalaity

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import "../plugins/flatpickr"
 import { loadDynamicBannerText } from '../components/banner';
-import {initAutocomplete} from "../plugins/init_autocomplete"
+import { initAutocomplete } from "../plugins/init_autocomplete"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 loadDynamicBannerText();

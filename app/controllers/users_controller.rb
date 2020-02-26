714b5fa82cdc35ca0c9c0b71dfa6d99b30ac5c31
James Loomos <jtloomos@gmail.com>
2020-02-26 16:20:16 -0300

    added edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,19 @@ class UsersController < ApplicationController
 
   def edit
     authorize(User.find(params[:id]))
+    @user = User.find(params[:id])
+  end
+
+  def update
+    authorize(User.find(params[:id]))
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    current_user.language_skills.destroy_all
+    params[:user][:languages].each do |language|
+      LanguageSkill.create(user_id: @user.id, language_id: language)
+    end
+    redirect_to users_dashboard_path
   end
 
   private
@@ -32,5 +45,11 @@ class UsersController < ApplicationController
     end
     return 0 if teacher.reviews.length.zero?
     sum.to_f / teacher.reviews.length
+  end
+
+  def user_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:user).permit(:first_name, :last_name, :email, :gender, :age, :phone_number, :location, :availability, :biography, :is_teacher, :languages)
   end
 end

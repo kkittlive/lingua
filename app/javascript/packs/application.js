abde9676b3379938687b429ec570544aca23676a
Emilie Drop <emilie.drop@gmx.fr>
2020-02-27 16:00:52 -0300

    maps added with style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@ import "../plugins/flatpickr"
 import { loadDynamicBannerText } from '../components/banner';
 import { initAutocomplete } from "../plugins/init_autocomplete"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 loadDynamicBannerText();
 

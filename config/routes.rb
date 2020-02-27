e0f4412313bb81d95f12aaa20a4d2b9e404bc9da
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-02-27 13:20:07 -0300

    Added all the different buttons, still testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
   resources :users, only: [:show, :edit, :update]
-  resources :lessons, only: [:create, :edit, :update]
+  resources :lessons, only: [:create, :edit, :update, :delete]
   resources :lessons do
     resources :reviews, only: %i[new create]
   end

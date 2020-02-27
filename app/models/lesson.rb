42ef2cb21f8d6d0430ec76ebcc61e547beebe516
James Loomos <jtloomos@gmail.com>
2020-02-27 16:56:26 -0300

    Changed spelling of cancelled

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@ class Lesson < ApplicationRecord
   belongs_to  :student, class_name: "User"
   belongs_to  :language
   has_many :reviews
-  STATE = %w[confirmed pending completed canceled]
+  STATE = %w[confirmed pending completed cancelled]
   validates :status, inclusion: {in: STATE }
   validates_presence_of :date, :location, :teacher, :student, :language
 end

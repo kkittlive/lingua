b1e328777c7df574097dd9ac1b2e3ea91347d768
Emilie Drop <emilie.drop@gmx.fr>
2020-02-27 15:46:57 -0300

    maps added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import "../plugins/flatpickr"
 import { loadDynamicBannerText } from '../components/banner';
 import { initAutocomplete } from "../plugins/init_autocomplete"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initMapbox } from '../plugins/init_mapbox';
 
 loadDynamicBannerText();
 
@@ -10,3 +12,4 @@ initUpdateNavbarOnScroll();
 
 initAutocomplete()
 
+initMapbox();
